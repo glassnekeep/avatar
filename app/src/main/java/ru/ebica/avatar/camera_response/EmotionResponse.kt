d131b4f2338a9bb5c080546296ea1e8a0ed2c5b3
Maxim Tsibikov <maksim.cibikov@iss.digital>
2024-11-30 00:55:33 +0300

    ui upd

--- app/src/main/java/ru/ebica/avatar/camera_response/EmotionResponse.kt
+++ app/src/main/java/ru/ebica/avatar/camera_response/EmotionResponse.kt
@@ -12,10 +12,26 @@ data class EmotionResponse(
         const val resultKey = "emotion_result_key"
         const val responseKey = "emotion_response_key"
 
-        fun generateRandomEmotionResult() = EmotionResponse(
-            emotion = Emotion.entries.toTypedArray().random(),
-            textToSpeak = ""
+        private val emotionWeights = mapOf(
+            Emotion.Angry to 5,
+            Emotion.Apathy to 10,
+            Emotion.Crying to 5,
+            Emotion.Happy to 20,
+            Emotion.Loved to 5,
+            Emotion.Sad to 20,
+            Emotion.Normal to 45
         )
+
+        fun generateRandomEmotionResult(): EmotionResponse {
+            val weightedEmotions = emotionWeights.flatMap { (emotion, weight) ->
+                List(weight) { emotion }
+            }
+            val randomEmotion = weightedEmotions.random()
+            return EmotionResponse(
+                emotion = randomEmotion,
+                textToSpeak = ""
+            )
+        }
     }
 
     enum class Emotion {
